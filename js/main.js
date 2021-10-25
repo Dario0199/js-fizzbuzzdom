3c7828f67b2b6f75833c5ae083058417896dd049
Dario0122 <dario.di.cuia@gmail.com>
2021-10-25 18:22:19 +0200

    multipli

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@ const box = document.querySelector('.box');
 
 for (let i = 1; i <= 100; i++){
     box.innerHTML += `<div class= "number number-${i}">${i}</div>`;
+
+    if ( (i % 3 == 0) && (i % 5 == 0) ){
+        box.innerHTML += `<div class= "number number-${i}">fizzbuzz</div>`;
+    } else if(i % 3 == 0) {
+        box.innerHTML += `<div class= "number number-${i} bg-green">fizz</div>`;
+    } else if (i % 5 == 0){
+        box.innerHTML += `<div class= "number number-${i}">buzz</div>`;
+    }  else {
+    }
 }
 
-if(box % 3 == 0) {
-    box.style.backgroundColor = 'green'
-
-}
