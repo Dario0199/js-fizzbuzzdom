92fa9bf477bf00eeef3e995ccf18f54dc2e49c92
Dario0122 <dario.di.cuia@gmail.com>
2021-10-25 16:09:30 +0200

    color

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ for (let i = 1; i <= 100; i++){
     box.innerHTML += `<div class= "number number-${i}">${i}</div>`;
 }
 
-if(box % 3 == 0) {
-    document.box.style.backgrundColor = 'green'
-
+if(i / 3 == 0) {
+    i.style.backgrundColor = 'green'
 }
