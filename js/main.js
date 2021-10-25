dec671f949c26cc2ccd1532b6b0d8b35ca615f4a
Dario0122 <dario.di.cuia@gmail.com>
2021-10-25 18:39:23 +0200

    color BG

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ for (let i = 1; i <= 100; i++){
     box.innerHTML += `<div class= "number number-${i}">${i}</div>`;
 
     if ( (i % 3 == 0) && (i % 5 == 0) ){
-        box.innerHTML += `<div class= "number number-${i}">fizzbuzz</div>`;
+        box.innerHTML += `<div class= "number number-${i} box bg-red">fizzbuzz</div>`;
     } else if(i % 3 == 0) {
-        box.innerHTML += `<div class= "number number-${i} bg-green">fizz</div>`;
+        box.innerHTML += `<div class= "number number-${i} box bg-green">fizz</div>`;
     } else if (i % 5 == 0){
-        box.innerHTML += `<div class= "number number-${i}">buzz</div>`;
+        box.innerHTML += `<div class= "number number-${i} box bg-yellow">buzz</div>`;
     }  else {
     }
 }
