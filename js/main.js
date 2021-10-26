afb93902dd5a5d29670f2f9aa5a87d4b0a74bbfb
Dario0122 <dario.di.cuia@gmail.com>
2021-10-26 10:21:43 +0200

    end

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-const box = document.querySelector('.box');
+const box = document.querySelector('.box')
 
-for (let i = 1; i <= 100; i++){
-    box.innerHTML += `<div class= "number number-${i}">${i}</div>`;
+for(let i = 1; i <= 100; i++){
 
-    if ( (i % 3 == 0) && (i % 5 == 0) ){
-        box.innerHTML += `<div class= "number number-${i} box bg-red">fizzbuzz</div>`;
-    } else if(i % 3 == 0) {
-        box.innerHTML += `<div class= "number number-${i} box bg-green">fizz</div>`;
+    let element = '';
+
+    if(i % 3 == 0 && i % 5 == 0){
+        element = '<div class="number bg-red">fizzbuzz</div>';
+    } else if (i % 3 == 0){
+        element = '<div class="number bg-green">fizz</div>';
     } else if (i % 5 == 0){
-        box.innerHTML += `<div class= "number number-${i} box bg-yellow">buzz</div>`;
-    }  else {
+        element = '<div class="number bg-yellow">buzz</div>';
+    } else {
+        element = `<div class="number">${i}</div>`;
     }
-}
 
+    box.innerHTML += element;
+}
